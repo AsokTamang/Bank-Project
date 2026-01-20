3cd8f94dfb16ddb0634ba686a5435b3259d19e67
AsokTamang <ashoktmg2058@gmail.com>
2026-01-19 23:11:07 -0500

    importing customers csv file

---
+++ main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_csv("C:/Users/ashok/Downloads/chapter8_assets/chapter8_assets/dataset/customers.csv")
+print(df)
+print(df.shape) #getting the number of rows and columns
+print(df.head(5))  #getting the overview of the data
+
