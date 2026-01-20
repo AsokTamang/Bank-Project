26555ae2f160166e44d505ec06a578b07acf4b0c
AsokTamang <ashoktmg2058@gmail.com>
2026-01-19 23:17:00 -0500

    reading the csv files of all the necessary datas of bank related to credit card

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
-df = pd.read_csv("C:/Users/ashok/Downloads/chapter8_assets/chapter8_assets/dataset/customers.csv")
-print(df)
-print(df.shape) #getting the number of rows and columns
-print(df.head(5))  #getting the overview of the data
-
+df_customers = pd.read_csv("C:/Users/ashok/Downloads/chapter8_assets/chapter8_assets/dataset/customers.csv")
+df_creditprofiles = pd.read_csv("C:/Users/ashok/Downloads/chapter8_assets/chapter8_assets/dataset/credit_profiles.csv")
+df_transactions = pd.read_csv("C:/Users/ashok/Downloads/chapter8_assets/chapter8_assets/dataset/transactions.csv")
+print(df_customers)
+print(df_customers.shape)
+df_customers.head(5)
+df_customers.occupation.unique()
+df_transactions.head(5)
+df_transactions.platform.unique()
