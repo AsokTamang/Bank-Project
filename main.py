f72778b335b9d49f7e8933d00638dc1fbce5a6e0
AsokTamang <ashoktmg2058@gmail.com>
2026-01-20 12:02:43 -0500

    checking the null values and the outliers for age column

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ df_transactions.platform.unique()
 
 #checking all the numeric datas
 print(df_customers.describe())
-q1a,q2a,q3a = df_customers.age.quantile([0.25,0.50,0.75])
+#q1a,q2a,q3a = df_customers.age.quantile([0.25,0.50,0.75])
 
 #checking the null values
 print(df_customers.isnull().sum()) #this helps us to check the null values in each column
@@ -33,12 +33,38 @@ print(df_customers[df_customers.annual_income.isnull()])
 occupations_median=df_customers.groupby('occupation').annual_income.median()
 print(occupations_median)  #here we are calculating the median of annual income of each and every occupations
 
-#filling the null values of a column annual income with the median of annual income based on the type of occupation.
+#cleaning the datas
+
+
+#1-filling the null values of a column annual income with the median of annual income based on the type of occupation.
 df_customers['annual_income'] = df_customers.apply(lambda row :occupations_median[row['occupation']] if pd.isna(row['annual_income']) else row['annual_income'],axis=1) #here axis=1 means we are using this apply method on each rows
 
+
+#ANNUAL INCOME COLUMN
 #data visualization
 plt.figure(figsize=(10,7))
 sns.histplot(df_customers['annual_income'],kde=True,label='data')
 plt.legend()
 plt.show()  #right skewed histogram of annual_income data
+
+#2-filling the outliers value with the appropriate median value
+print(df_customers.annual_income.describe())
+#as we can see the minimum value of annual_income is 2, which is surely a bug data
+#so to clean this , we have assumed 5000 as the annual income of a person
+for index,row in df_customers.iterrows():
+    if row.annual_income<5000:  #if the annual income in the current row is less than 5000 then we are filling this value with the median
+        df_customers.at[index,'annual_income']=occupations_median[row.occupation]
+#checking the value
+print(df_customers.iloc[[31]])
+
+
+#AGE COLUMN
+df_customers.age.isnull().sum() #checking the null values in the age column
+(df_customers.age<15).sum()  #calculating the number of rows having age less than 15
+(df_customers.age>80).sum()  #calculating the total number of rows having age more than 80
+df_customers.age.describe()
+median_age = df_customers.age.median()
+print(median_age)
+
+
 
