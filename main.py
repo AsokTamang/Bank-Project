6c8a395783a0741f15029e0b2ca108a6e60b5393
AsokTamang <ashoktmg2058@gmail.com>
2026-01-21 00:10:06 -0500

    cleaned the transaction amount column data in transactions dataframe

--- main.py
+++ main.py
@@ -200,8 +200,12 @@ print(df_zero_transaction.platform)
 print(df_zero_transaction.product_category)
 print(df_zero_transaction.payment_type)
 
+#finding the required replacement median value
 df_new_transactions= df_transactions[(df_transactions['platform'] =="Amazon") & (df_transactions['product_category'] =="Electronics") & (df_transactions['payment_type'] =="Credit Card")]
 replacing_tran_amount= df_new_transactions[df_new_transactions.tran_amount>0]['tran_amount'].median()  #as for the category, platform and payment type of the row having tran_amount 0 is same,
 #we are using the median of tran amount of the row where the tran amount is greater than 0, and having that matched category, platform and payment type inorder to replace the 0 tran amount column data
 
-print(df_transactions[df_transactions.tran_amount==0].shape)
+print(df_transactions[df_transactions.tran_amount==0].shape)  #number of rows having transaction amount equal to 0
+df_transactions.loc[df_transactions.tran_amount==0,'tran_amount'] = replacing_tran_amount  #here we are selecting the rows and columns as df_transactions.tran_amount==0,'tran_amount'  replacing it with replacing_tran_amount
+
+print(df_transactions[df_transactions.tran_amount==0].shape)  #number of rows having transaction amount equal to 0 after cleaning the data, and the output is 0
