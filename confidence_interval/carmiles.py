50148fa0240db66542c2712cbdf1915d4834d7da
AsokTamang <ashoktmg2058@gmail.com>
2026-01-22 13:26:22 -0500

    using scipy norm to calculate zscore from probability percent value in decimal

--- confidence_interval/carmiles.py
+++ confidence_interval/carmiles.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy.stats import norm
 
 df=pd.read_csv("C:/Users/ashok/Downloads/chapter9_assets/chapter9_assets/8_Confidence Interval Estimate Car Miles/miles.csv")
 plt.figure(figsize = (12,8))
@@ -24,6 +25,8 @@ print(population_mean)
 
 
 
-
+z_score = norm.ppf(0.975)  #converting the probability percent value into zscore
+margin_error = z_score * (population_std/np.sqrt(n))
+print(margin_error)
 
 
