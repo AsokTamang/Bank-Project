cb52869f82e28c3e90a1957fb5e0c418d4d41ac6
AsokTamang <ashoktmg2058@gmail.com>
2026-01-23 12:14:39 -0500

    calculating sample, sample mean and population standard deviation

--- exercise/housing_inflation_test.py
+++ exercise/housing_inflation_test.py
@@ -6,4 +6,16 @@ from scipy.stats import norm
 df = pd.read_csv("C:/Users/ashok/Downloads/chapter10_assets/chapter10_assets/3_z_test_housing_inflation/house_price_increase.csv")
 
 population_mean = df.price_increase_pct.mean()
+population_std = df.price_increase_pct.std()
 print(population_mean)
+
+
+#calculating the sample
+n = 50
+df_sample = df.sample(50,random_state = 11)
+print(df_sample)
+
+sample_mean = df_sample.price_increase_pct.mean()
+print(sample_mean)
+
+
