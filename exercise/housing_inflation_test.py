75914bce7f8ae8dea02ddea2a119d9cdd0325b9a
AsokTamang <ashoktmg2058@gmail.com>
2026-01-23 12:08:42 -0500

    housing inflation testing

---
+++ exercise/housing_inflation_test.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import norm
+
+df = pd.read_csv("C:/Users/ashok/Downloads/chapter10_assets/chapter10_assets/3_z_test_housing_inflation/house_price_increase.csv")
+
