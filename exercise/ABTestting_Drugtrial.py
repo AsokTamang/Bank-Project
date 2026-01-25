dd18dc865c0ce55514b688bcaa7d92db7dd95e5d
AsokTamang <ashoktmg2058@gmail.com>
2026-01-25 18:01:24 -0500

    calculating the mean and std deviation of recovery time from control group using old drug

--- exercise/ABTestting_Drugtrial.py
+++ exercise/ABTestting_Drugtrial.py
@@ -4,3 +4,8 @@ print(df_control)
 
 df_test = pd.read_csv("C:/Users/ashok/Downloads/chapter10_assets/chapter10_assets/11_z_test_AB_testing_coding/test_group.csv")
 print(df_test)
+
+control_mean = df_control.recovery_time_hrs.mean()
+control_std = df_control.recovery_time_hrs.std()
+print(control_mean)
+print(control_std)
