1dcc7f2ed91e8412af2c395cead0dfa5dd561f7c
AsokTamang <ashoktmg2058@gmail.com>
2026-01-25 18:08:26 -0500

    calculating the z_score of two distrubutions in a single metric

--- exercise/ABTestting_Drugtrial.py
+++ exercise/ABTestting_Drugtrial.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 df_control = pd.read_csv("C:/Users/ashok/Downloads/chapter10_assets/chapter10_assets/11_z_test_AB_testing_coding/control_group.csv")
 print(df_control)
 
@@ -14,3 +15,10 @@ test_mean = df_test.recovery_time_hrs.mean()
 test_std = df_test.recovery_time_hrs.std()
 print(test_std)
 print(test_mean)
+
+control_size = df_control.shape[0]  #here we are getting the shape of control group
+test_size = df_test.shape[0]  #here we are getting the size of test group
+
+a = control_std ** 2 / control_size
+b = test_std ** 2 / test_size
+z_score = (control_mean - test_mean) / np.sqrt(a+b)
