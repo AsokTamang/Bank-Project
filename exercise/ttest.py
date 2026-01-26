1aaca324ef0a4e864177ceb30c0b47bd8a4f3049
AsokTamang <ashoktmg2058@gmail.com>
2026-01-26 11:53:27 -0500

    calculating the degree of freedom

--- exercise/ttest.py
+++ exercise/ttest.py
@@ -3,8 +3,12 @@ import numpy as np
 from scipy.stats import norm , t
 
 population_mean = 72  #given from question
+alpha = 0.05
 
 df = pd.read_csv("C:/Users/ashok/Downloads/chapter12_assets/chapter12_assets/12_2_case_study_exam_score/exam_scores.csv")
 print(df)
 print(df.shape)
 
+n=df.shape[0]  #sample size
+degree_of_freedom =n-1
+print(degree_of_freedom)
